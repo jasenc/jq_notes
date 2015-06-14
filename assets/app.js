a3be57e4743845beddfb4522b467ab89c33c5e5b
jasenc <jasen.c8@gmail.com>
2015-06-14 16:35:53 -0700

    refactored

--- assets/app.js
+++ assets/app.js
@@ -20,12 +20,68 @@ function searching() {
   $btnClear.show();
   $btnNew.hide();
   $note.hide();
+  var search = $search.val();
+  $note = $('.note');
+  $(".note:contains('" + search + "')").show();
 }
 
 function clearing() {
   $btnClear.hide();
   $btnNew.show();
   $note.show();
+  $search.val('');
+}
+
+function toNewNote() {
+  $allNotes.toggle();
+  $newNote.toggle();
+}
+
+function toEditNote() {
+  $allNotes.toggle();
+  $editNote.toggle();
+}
+
+function disableSearch() {
+  $btnSearch.prop('disabled', true);
+  $search.attr("placeholder", "Sorry, can't search while you're making a new note!");
+  $search.prop('disabled', true);
+}
+
+function enableSearch() {
+  $search.prop('disabled', false);
+  $search.attr("placeholder", "Search by title, tags, date, or even words/sentences in notes");
+  $btnSearch.prop('disabled', false);
+}
+
+function warnings() {
+  if (title.length < 1) {
+    $titleWarn.show();
+  }
+  if (tags.length < 1) {
+    $tagsWarn.show();
+  }
+  if (notes.length < 1) {
+    $noteWarn.show();
+  }
+}
+
+function closeWarnings() {
+  $titleWarn.hide();
+  $tagsWarn.hide();
+  $noteWarn.hide();
+}
+
+function clearNote() {
+  title = $('#title').val('');
+  tags = $('#tags').val('');
+  notes = $('#notes').val('');
+}
+
+function checkNotes() {
+  title = $('#title').val();
+  tags = $('#tags').val();
+  notes = $('#notes').val();
 }
 
 function main() {
@@ -42,73 +98,40 @@ function main() {
 
   $btnSearch.on('click', function(){
     searching($(this));
-    var search = $search.val();
-    $note = $('.note');
-    $(".note:contains('" + search + "')").show();
   });
 
   $btnClear.on('click', function() {
-    $search.val('');
     clearing($(this));
   })
 
   $btnNew.on('click', function(){
-    title = $('#title').val();
-    tags = $('#tags').val();
-    notes = $('#notes').val();
-    $allNotes.hide();
-    $newNote.show();
-    $btnSearch.prop('disabled', true);
-    $search.attr("placeholder", "Sorry, can't search while you're making a new note!");
-    $search.prop('disabled', true);
-    $editNote.hide();
-    // Work on this!
-    if ($editNote.is(':visisble') == true) {
-      alert('Your changes have been lost, this is something I will work on');
-    };
+    checkNotes($(this));
+    disableSearch($(this));
+    toNewNote($(this));
+    $btnNew.prop('disabled', true);
   });
 
   $('#post').on('click', function(){
     myDate = new Date();
     if(title.length < 1 && tags.length < 1 && notes.length < 1){
-      title = $('#title').val();
-      tags = $('#tags').val();
-      notes = $('#notes').val();
+      checkNotes($(this));
     }
-    if (title.length < 1) {
-      $titleWarn.show();
-    }
-    if (tags.length < 1) {
-      $tagsWarn.show();
-    }
-    if (notes.length < 1) {
-      $noteWarn.show();
-    }
+    warnings($(this));
     if (title.length >= 1 && tags.length >= 1 && notes.length >= 1) {
       $allNotes.prepend('<li class="note"><div><h1>' + title + '</h1><div class="date"><h2>'+ myDate.toDateString() +'</h2><button class="btn btn-edit">Edit</button></div><h3>' + tags + '</h3><p>' + notes + '</p></div></li>');
-      $allNotes.show();
-      $newNote.hide();
-      $titleWarn.hide();
-      $tagsWarn.hide();
-      $noteWarn.hide();
-      $search.prop('disabled', false);
-      $search.attr("placeholder", "Search by title, tags, date, or even words/sentences in notes");
-      $btnSearch.prop('disabled', false);
-      title = $('#title').val('');
-      tags = $('#tags').val('');
-      notes = $('#notes').val('');
+      toNewNote($(this));
+      closeWarnings($(this));
+      enableSearch($(this));
+      clearNote($(this));
+      $btnNew.prop('disabled', false);
     }
   });
 
   $('.btn-save').on('click', function(){
-    $allNotes.show();
-    $newNote.hide();
-    $btnSearch.prop('disabled', false);
-    $search.attr("placeholder", "Search by title, tags, date, or even words/sentences in notes");
-    $search.prop('disabled', false);
-    $titleWarn.hide();
-    $tagsWarn.hide();
-    $noteWarn.hide();
+    toNewNote($(this));
+    enableSearch($(this));
+    closeWarnings($(this));
+    $btnNew.prop('disabled', false);
   });
 
 // Use on document to work with dynamically added DOM elements
@@ -124,42 +147,28 @@ function main() {
 
   $(document).on('click', '.btn-edit', function(){
     $(this).parents('.note').addClass('edited-note');
-    $allNotes.hide();
-    $editNote.show();
-    $btnSearch.prop('disabled', true);
-    $search.attr("placeholder", "Sorry, can't search while you're making a new note!");
-    $search.prop('disabled', true);
+    toEditNote($(this));
+    disableSearch($(this));
     $('#edit-title').val($('.edited-note').find('h1').text());
     $('#edit-tags').val($('.edited-note').find('h3').text());
     $('#edit-notes').val($('.edited-note').find('p').text());
     myDate = $('.edited-note').find('h2').text();
+    $btnNew.prop('disabled', true);
   });
 
   $('#edit').on('click', function(){
     title = $('#edit-title').val();
     tags = $('#edit-tags').val();
     notes = $('#edit-notes').val();
-    if (title.length < 1) {
-      $titleWarn.show();
-    }
-    if (tags.length < 1) {
-      $tagsWarn.show();
-    }
-    if (notes.length < 1) {
-      $noteWarn.show();
-    }
+    warnings($(this));
     if (title.length >= 1 && tags.length >= 1 && notes.length >= 1) {
       $('.edited-note').html('<div><h1>' + title + '</h1><div class="date"><h2>'+ myDate +'</h2><span class="btn btn-edit">Edit</span></div><h3>' + tags + '</h3><p>' + notes + '</p></div>');
-      $allNotes.show();
-      $editNote.hide();
-      $titleWarn.hide();
-      $tagsWarn.hide();
-      $noteWarn.hide();
-      $search.prop('disabled', false);
-      $search.attr("placeholder", "Search by title, tags, date, or even words/sentences in notes");
-      $btnSearch.prop('disabled', false);
+      toEditNote($(this));
+      closeWarnings($(this));
+      enableSearch($(this));
       $('.edited-note').removeClass('edited-note');
     }
+    $btnNew.prop('disabled', false);
   });
 
   $btnDelete.on('click', function(){
@@ -171,9 +180,8 @@ function main() {
     $('#title').val('');
     $('#tags').val('');
     $('#notes').val('');
-    $search.prop('disabled', false);
-    $search.attr("placeholder", "Search by title, tags, date, or even words/sentences in notes");
-    $btnSearch.prop('disabled', false);
+    enableSearch($(this));
+    $btnNew.prop('disabled', false);
   });
 
 }
